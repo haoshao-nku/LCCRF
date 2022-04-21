aa13030c5b16155c757c377d4826810e2ab6f298
Alexander-Shao <55780822+Alexander-Shao@users.noreply.github.com>
2022-04-21 19:28:57 +0800

    Add files via upload

---
+++ eval.py
@@ -0,0 +1,155 @@
+"""
+Angular Triplet Loss
+YE, Hanrong et al, Bi-directional Exponential Angular Triplet Loss for RGB-Infrared Person Re-Identification
+"""
+from __future__ import print_function, absolute_import
+import numpy as np
+import copy
+from collections import defaultdict
+import sys
+import torch
+import matplotlib.pyplot as plt
+import pickle
+
+
+from IPython import embed
+
+
+
+def test(feature_generators, queryloader, galleryloader, use_gpu = True, ranks=[1, 5, 10, 20]):
+    if type(feature_generators) is list:
+        feature_generator_rgb = feature_generators[0]
+        feature_generator_ir = feature_generators[1]
+
+    else:
+        feature_generator_rgb = feature_generators
+        feature_generator_ir = feature_generators
+    feature_generator_rgb.eval()
+    feature_generator_ir.eval()
+
+    with torch.no_grad():
+        qf, q_pids, q_camids = [], [], []
+        for batch_idx, (imgs, pids, camids) in enumerate(queryloader):
+            if use_gpu: imgs = imgs.cuda()
+            features = feature_generator_ir(imgs) # query features # use fi   
+            features = features.data#.cpu()
+
+            qf.append(features)
+            q_pids.extend(pids)
+            q_camids.extend(camids)
+
+        qf = torch.cat(qf, 0)
+        q_pids = np.asarray(q_pids)
+        q_camids = np.asarray(q_camids)
+
+        print("Extracted features for query set, obtained {}-by-{} matrix".format(qf.size(0), qf.size(1)))
+
+        gf, g_pids, g_camids = [], [], []
+        #end = time.time()
+        for batch_idx, (imgs, pids, camids) in enumerate(galleryloader):
+            if use_gpu: imgs = imgs.cuda()
+
+            features = feature_generator_rgb(imgs)
+            features = features.data#.cpu()
+
+            gf.append(features)
+            g_pids.extend(pids)
+            g_camids.extend(camids)
+        gf = torch.cat(gf, 0)
+        g_pids = np.asarray(g_pids)
+        g_camids = np.asarray(g_camids)
+
+        print("Extracted features for gallery set, obtained {}-by-{} matrix".format(gf.size(0), gf.size(1)))
+
+
+    qf = qf.view(qf.size(0),-1)
+    gf = gf.view(gf.size(0),-1)
+
+    # see norm
+    q_norms = qf.norm(dim=1)
+    print('q_norms:')
+    print(q_norms)
+
+    g_norms = gf.norm(dim=1)
+    print('g_norms:')
+    print(g_norms)
+    m, n = qf.size(0), gf.size(0)
+    distmat = torch.pow(qf, 2).sum(dim=1, keepdim=True).expand(m, n) + \
+              torch.pow(gf, 2).sum(dim=1, keepdim=True).expand(n, m).t()
+    distmat.addmm_(1, -2, qf, gf.t())
+    distmat = distmat.cpu().numpy()
+
+    print("Computing CMC and mAP")
+    cmc, mAP = evaluate(distmat, q_pids, g_pids, q_camids, g_camids) # use_metric_cuhk03=args.use_metric_cuhk03)
+
+    print("Results ----------")
+    print("mAP: {:.1%}".format(mAP))
+    print("CMC curve")
+    for r in ranks:
+        print("Rank-{:<3}: {:.1%}".format(r, cmc[r-1]))
+    print("------------------")
+
+    return distmat,cmc, mAP 
+
+def evaluate(distmat, q_pids, g_pids, q_camids, g_camids, max_rank=50):
+    """Evaluation with SYSU metric
+    Key: for each query identity in camera 3, its gallery images from camera 2 view are discarded.
+    """
+    
+    num_q, num_g = distmat.shape
+    if num_g < max_rank:
+        max_rank = num_g
+        print("Note: number of gallery samples is quite small, got {}".format(num_g))
+    indices = np.argsort(distmat, axis=1)
+
+    matches = (g_pids[indices] == q_pids[:, np.newaxis]).astype(np.int32)
+
+    # compute cmc curve for each query
+    all_cmc = []
+    all_AP = []
+    
+    num_valid_q = 0. # number of valid query
+    for q_idx in range(num_q):
+        # get query pid and camid
+        q_pid = q_pids[q_idx]
+        q_camid = q_camids[q_idx]
+        # remove gallery samples that have the same pid and camid with query
+        order = indices[q_idx]
+        remove = (q_camid == 3) & (g_camids[order] == 2)
+        keep = np.invert(remove)
+        
+
+        if(not q_idx):
+            print('Query ID',q_pid)
+            for g_idx in range(20):
+                print('Gallery ID Rank #', g_idx ,' : ', g_pids[order[g_idx]], 'distance : ', distmat[q_idx][order[g_idx]])
+
+        # compute cmc curve
+        orig_cmc = matches[q_idx][keep] # binary vector, positions with value 1 are correct matches
+        if not np.any(orig_cmc):
+            # this condition is true when query identity does not appear in gallery
+            continue
+
+        cmc = orig_cmc.cumsum()
+        cmc[cmc > 1] = 1
+
+        all_cmc.append(cmc[:max_rank])
+        num_valid_q += 1.
+
+        # compute average precision
+        # reference: https://en.wikipedia.org/wiki/Evaluation_measures_(information_retrieval)#Average_precision
+        num_rel = orig_cmc.sum()
+        tmp_cmc = orig_cmc.cumsum()
+        tmp_cmc = [x / (i+1.) for i, x in enumerate(tmp_cmc)]
+
+        tmp_cmc = np.asarray(tmp_cmc) * orig_cmc
+        AP = tmp_cmc.sum() / num_rel
+        all_AP.append(AP)
+
+    assert num_valid_q > 0, "Error: all query identities do not appear in gallery"
+
+    all_cmc = np.asarray(all_cmc).astype(np.float32)
+    all_cmc = all_cmc.sum(0) / num_valid_q
+    mAP = np.mean(all_AP)
+    return all_cmc, mAP
+
---
+++ models.py
@@ -0,0 +1,236 @@
+"""
+Angular Triplet Loss
+YE, Hanrong et al, Bi-directional Exponential Angular Triplet Loss for RGB-Infrared Person Re-Identification
+"""
+
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models
+from backbone import ResNet
+import settings
+import torch
+import math
+
+class Normalize(nn.Module):
+    def __init__(self, power=2):
+        super(Normalize, self).__init__()
+        self.power = power
+
+    def forward(self, x):
+        norm = x.pow(self.power).sum(1, keepdim=True).pow(1. / self.power)
+        out = x.div(norm)
+        return out
+
+
+class Non_local(nn.Module):
+    def __init__(self, in_channels, reduc_ratio=2):
+        super(Non_local, self).__init__()
+
+        self.in_channels = in_channels
+        self.inter_channels = reduc_ratio//reduc_ratio
+
+        self.g = nn.Sequential(
+            nn.Conv2d(in_channels=self.in_channels, out_channels=self.inter_channels, kernel_size=1, stride=1,
+                    padding=0),
+        )
+
+        self.W = nn.Sequential(
+            nn.Conv2d(in_channels=self.inter_channels, out_channels=self.in_channels,
+                    kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(self.in_channels),
+        )
+        nn.init.constant_(self.W[1].weight, 0.0)
+        nn.init.constant_(self.W[1].bias, 0.0)
+
+
+
+        self.theta = nn.Conv2d(in_channels=self.in_channels, out_channels=self.inter_channels,
+                             kernel_size=1, stride=1, padding=0)
+
+        self.phi = nn.Conv2d(in_channels=self.in_channels, out_channels=self.inter_channels,
+                           kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x):
+        '''
+                :param x: (b, c, t, h, w)
+                :return:
+                '''
+
+        batch_size = x.size(0)
+        g_x = self.g(x).view(batch_size, self.inter_channels, -1)
+        g_x = g_x.permute(0, 2, 1)
+
+        theta_x = self.theta(x).view(batch_size, self.inter_channels, -1)
+        theta_x = theta_x.permute(0, 2, 1)
+        phi_x = self.phi(x).view(batch_size, self.inter_channels, -1)
+        f = torch.matmul(theta_x, phi_x)
+        N = f.size(-1)
+        # f_div_C = torch.nn.functional.softmax(f, dim=-1)
+        f_div_C = f / N
+
+        y = torch.matmul(f_div_C, g_x)
+        y = y.permute(0, 2, 1).contiguous()
+        y = y.view(batch_size, self.inter_channels, *x.size()[2:])
+        W_y = self.W(y)
+        z = W_y + x
+
+        return z
+
+class FeatureEmbedder(nn.Module):
+    def __init__(self,last_stride,model_path,part=3):
+        super(FeatureEmbedder, self).__init__()
+        #self.gap = nn.AdaptiveAvgPool2d(1)
+        self.bottleneck = nn.BatchNorm1d(2048)
+        self.bottleneck.bias.requires_grad_(False)  # no shift
+        self.bottleneck.apply(weights_init_kaiming)
+        self.base = ResNet(last_stride)
+        self.base.load_param(model_path)
+        layers = [3, 4, 6, 3]
+        non_layers = [0, 2, 3, 0]
+        self.part = part
+        self.NL_2 = nn.ModuleList(
+            [Non_local(512) for i in range(non_layers[1])])
+        self.NL_2_idx = sorted([layers[1] - (i + 1) for i in range(non_layers[1])])
+        self.NL_3 = nn.ModuleList(
+            [Non_local(1024) for i in range(non_layers[2])])
+        self.NL_3_idx = sorted([layers[2] - (i + 1) for i in range(non_layers[2])])
+        self.NL_4 = nn.ModuleList(
+            [Non_local(2048) for i in range(non_layers[3])])
+        self.NL_4_idx = sorted([layers[3] - (i + 1) for i in range(non_layers[3])])
+    def forward(self, x):
+        NL2_counter = 0
+        if len(self.NL_2_idx) == 0: self.NL_2_idx = [-1]
+        for i in range(len(self.base.layer2)):
+            x = self.base.layer2[i](x)
+            if i == self.NL_2_idx[NL2_counter]:
+                _, C, H, W = x.shape
+                x = self.NL_2[NL2_counter](x)
+                NL2_counter += 1
+        # Layer 3
+        NL3_counter = 0
+        if len(self.NL_3_idx) == 0: self.NL_3_idx = [-1]
+        for i in range(len(self.base.layer3)):
+            x = self.base.layer3[i](x)
+            if i == self.NL_3_idx[NL3_counter]:
+                _, C, H, W = x.shape
+                x = self.NL_3[NL3_counter](x)
+                NL3_counter += 1
+        # Layer 4
+        NL4_counter = 0
+        if len(self.NL_4_idx) == 0: self.NL_4_idx = [-1]
+        for i in range(len(self.base.layer4)):
+            x = self.base.layer4[i](x)
+            if i == self.NL_4_idx[NL4_counter]:
+                _, C, H, W = x.shape
+                x = self.NL_4[NL4_counter](x)
+                NL4_counter += 1
+        b, c, h, w = x.shape
+        y = x.view(b, c, -1)
+        p = 3.0
+        global_feat = (torch.mean(y ** p, dim=-1) + 1e-12) ** (1 / p)
+        feat = global_feat.view(global_feat.shape[0], -1)  # flatten to (bs, 2048)
+        bnfeat = self.bottleneck(feat)  # normalize for angular softmax
+        return bnfeat
+
+class IdClassifier(nn.Module):
+    def __init__(self, in_planes = 2048, num_classes = settings.num_classes): # train 296, val 99
+        super(IdClassifier, self).__init__()
+        self.classifier = nn.Linear(in_planes, num_classes, bias=False)
+        self.classifier.apply(weights_init_classifier)
+        self.dropout = 0.5
+        self.l2norm = Normalize(2)
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        if self.training:
+            x = F.dropout(x,self.dropout,training = self.training)
+            x = F.elu(x)
+        else :
+            x =  self.l2norm(x)
+        out = self.classifier(x)
+        return out
+
+def weights_init_kaiming(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        nn.init.kaiming_normal_(m.weight, a=0, mode='fan_out')
+        nn.init.constant_(m.bias, 0.0)
+    elif classname.find('Conv') != -1:
+        nn.init.kaiming_normal_(m.weight, a=0, mode='fan_in')
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0.0)
+    elif classname.find('BatchNorm') != -1:
+        if m.affine:
+            nn.init.constant_(m.weight, 1.0)
+            nn.init.constant_(m.bias, 0.0)
+
+
+def weights_init_classifier(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        nn.init.normal_(m.weight, std=0.001)
+        if m.bias:
+            nn.init.constant_(m.bias, 0.0)
+
+
+class Baseline(nn.Module):
+    def __init__(self, last_stride, model_path):
+        super(Baseline, self).__init__()
+        self.base = ResNet(last_stride)
+        self.base.load_param(model_path)
+
+    def forward(self, x):
+        return  self.base(x)  # (b, 2048, 1, 1)
+
+class Base_rgb(nn.Module):
+    def __init__(self, last_stride, model_path):
+        super(Base_rgb, self).__init__()
+        self.base = ResNet(last_stride)
+        self.base.load_param(model_path)
+
+        layers = [3, 4, 6, 3]
+        non_layers = [0, 2, 3, 0]
+        self.NL_1 = nn.ModuleList(
+            [Non_local(256) for i in range(non_layers[0])])
+        self.NL_1_idx = sorted([layers[0] - (i + 1) for i in range(non_layers[0])])
+    def forward(self, x):
+
+        x = self.base.conv1(x)
+        x = self.base.bn1(x)
+        x = self.base.relu(x)
+        x = self.base.maxpool(x)
+        NL1_counter = 0
+        if len(self.NL_1_idx) == 0: self.NL_1_idx = [-1]
+        for i in range(len(self.base.layer1)):
+            x = self.base.layer1[i](x)
+            if i == self.NL_1_idx[NL1_counter]:
+                _, C, H, W = x.shape
+                x = self.NL_1[NL1_counter](x)
+                NL1_counter += 1
+        return  x
+
+class Base_ir(nn.Module):
+    def __init__(self, last_stride, model_path):
+        super(Base_ir, self).__init__()
+        self.base = ResNet(last_stride)
+        self.base.load_param(model_path)
+
+        layers = [3, 4, 6, 3]
+        non_layers = [0, 2, 3, 0]
+        self.NL_1 = nn.ModuleList(
+            [Non_local(256) for i in range(non_layers[0])])
+        self.NL_1_idx = sorted([layers[0] - (i + 1) for i in range(non_layers[0])])
+    def forward(self, x):
+
+        x = self.base.conv1(x)
+        x = self.base.bn1(x)
+        x = self.base.relu(x)
+        x = self.base.maxpool(x)
+        NL1_counter = 0
+        if len(self.NL_1_idx) == 0: self.NL_1_idx = [-1]
+        for i in range(len(self.base.layer1)):
+            x = self.base.layer1[i](x)
+            if i == self.NL_1_idx[NL1_counter]:
+                _, C, H, W = x.shape
+                x = self.NL_1[NL1_counter](x)
+                NL1_counter += 1
+        return  x
